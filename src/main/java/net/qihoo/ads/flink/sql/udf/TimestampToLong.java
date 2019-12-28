6bb9782dc4ddf11593ee748f5d428c9f437556e0
zhuguangbin <zhuguangbin@360.cn>
2019-12-28 20:37:20 +0800

    bugfix for TimestampToLong udf

--- src/main/java/net/qihoo/ads/flink/sql/udf/TimestampToLong.java
+++ src/main/java/net/qihoo/ads/flink/sql/udf/TimestampToLong.java
@@ -3,9 +3,13 @@ package net.qihoo.ads.flink.sql.udf;
 import org.apache.flink.table.functions.ScalarFunction;
 
 import java.sql.Timestamp;
+import java.util.TimeZone;
 
 public class TimestampToLong extends ScalarFunction {
-    public Long eval(Timestamp input) {
-        return input.getTime();
+    private static final TimeZone LOCAL_TZ = TimeZone.getDefault();
+
+    public Long eval(Timestamp ts) {
+        long time = ts.getTime();
+        return time + LOCAL_TZ.getOffset(time);
     }
 }
