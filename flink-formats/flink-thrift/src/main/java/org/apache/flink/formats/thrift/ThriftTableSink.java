7e7d4e30f2a3a47d369098b557befe238778193e
zhuguangbin <zhuguangbin@360.cn>
2020-07-29 19:35:59 +0800

    add elephant-bird support for thrift format

--- flink-formats/flink-thrift/src/main/java/org/apache/flink/formats/thrift/ThriftTableSink.java
+++ flink-formats/flink-thrift/src/main/java/org/apache/flink/formats/thrift/ThriftTableSink.java
@@ -35,7 +35,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.List;
 
 /**
  * Table sink for storing data in thrift format.
---
+++ flink-formats/flink-thrift/src/main/java/org/apache/flink/formats/thrift/elephantbird/ElephantBirdThriftWriters.java
@@ -0,0 +1,40 @@
+package org.apache.flink.formats.thrift.elephantbird;
+
+import org.apache.flink.formats.thrift.ThriftBuilder;
+import org.apache.flink.formats.thrift.ThriftWriterFactory;
+
+import com.hadoop.compression.lzo.LzopCodec;
+import com.twitter.elephantbird.mapreduce.io.RawBlockWriter;
+import com.twitter.elephantbird.mapreduce.io.ThriftBlockWriter;
+import org.apache.hadoop.io.compress.CompressionOutputStream;
+import org.apache.thrift.TBase;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ElephantBirdThriftWriters {
+
+    public static <T extends TBase<?, ?>> ThriftWriterFactory<T> forThriftBlock(Class<T> type) {
+
+        final ThriftBuilder<T> builder = (out) -> createThriftBlockWriter(type, out);
+        return new ThriftWriterFactory<>(builder);
+    }
+
+    public static ThriftWriterFactory<byte[]> forLzoRawBlock() {
+        final ThriftBuilder<byte[]> builder = (out) -> createLzoRawBlockWriter(out);
+        return new ThriftWriterFactory<byte[]>(builder);
+    }
+
+    private static <T> ThriftBlockWriter createThriftBlockWriter(
+            Class<T> type,
+            OutputStream out) {
+
+        return new ThriftBlockWriter(out, type);
+    }
+
+    private static RawBlockWriter createLzoRawBlockWriter(OutputStream out) throws IOException {
+        LzopCodec codec = new LzopCodec();
+        CompressionOutputStream compressOut = codec.createOutputStream(out);
+        return new RawBlockWriter(compressOut);
+    }
+}
